cb2ef9296766dacffb66661b8705c1d177d4e49f
Josh Friedlander <16547083+lordgrenville@users.noreply.github.com>
2023-01-12 15:37:00 +0200

    make import explicit, improve setup.py, format

--- scraper_test.py
+++ scraper_test.py
@@ -1,50 +1,57 @@
 from itunes_app_scraper.scraper import AppStoreScraper
-from itunes_app_scraper.util import AppStoreException, AppStoreCollections, AppStoreCategories, AppStoreUtils
+from itunes_app_scraper.util import AppStoreException
 
-import json
 import pytest
 import os
+
 
 def test_term_no_exception():
     scraper = AppStoreScraper()
     results = scraper.get_app_ids_for_query("mindful", country="gb", lang="en")
     assert len(results) > 0
 
+
 def test_no_term_gives_exception():
     scraper = AppStoreScraper()
-    with pytest.raises(AppStoreException, match = "No term was given"):
+    with pytest.raises(AppStoreException, match="No term was given"):
         scraper.get_app_ids_for_query("", country="gb", lang="en")
+
 
 def test_no_invalid_id_gives_exception():
     scraper = AppStoreScraper()
-    with pytest.raises(AppStoreException, match = "No app found with ID 872"):
-        scraper.get_app_details('872')
+    with pytest.raises(AppStoreException, match="No app found with ID 872"):
+        scraper.get_app_details("872")
+
 
 def test_no_invalid_id_in_multiple_is_empty():
     scraper = AppStoreScraper()
-    assert len(list(scraper.get_multiple_app_details(['872']))) == 0
+    assert len(list(scraper.get_multiple_app_details(["872"]))) == 0
+
 
 def test_no_invalid_id_in_multiple_writes_log():
     scraper = AppStoreScraper()
-    scraper.get_multiple_app_details(['872'])
+    scraper.get_multiple_app_details(["872"])
     assert os.path.exists("log/nl_log.txt")
-    fh = open('log/nl_log.txt')
+    fh = open("log/nl_log.txt")
     assert "No app found with ID 872" in fh.read()
     fh.close()
 
+
 def test_log_file_write_message():
     scraper = AppStoreScraper()
-    scraper._log_error("gb","test")
+    scraper._log_error("gb", "test")
     assert os.path.exists("log/gb_log.txt")
-    fh = open('log/gb_log.txt')
+    fh = open("log/gb_log.txt")
     assert "test" in fh.read()
     fh.close()
 
+
 def test_country_code_does_exist():
     scraper = AppStoreScraper()
-    assert scraper.get_store_id_for_country('gb') == 143444
+    assert scraper.get_store_id_for_country("gb") == 143444
+
 
 def test_country_code_does_not_exist():
     scraper = AppStoreScraper()
     with pytest.raises(AppStoreException, match="Country code not found for XZ"):
-        scraper.get_store_id_for_country('xz')
+        scraper.get_store_id_for_country("xz")
--- util_test.py
+++ util_test.py
@@ -1,26 +1,38 @@
-from itunes_app_scraper.util import AppStoreException, AppStoreCollections, AppStoreCategories, AppStoreUtils
+from itunes_app_scraper.util import (
+    AppStoreCollections,
+    AppStoreCategories,
+    AppStoreUtils,
+)
 
 import json
 import pytest
-import os
+
 
 def test_category_exists():
     category = AppStoreCategories()
     assert category.BOOKS == 6018
 
+
 def test_category_does_not_exist():
     category = AppStoreCategories()
-    with pytest.raises(AttributeError, match="'AppStoreCategories' object has no attribute 'METHOD'"):
+    with pytest.raises(
+        AttributeError, match="'AppStoreCategories' object has no attribute 'METHOD'"
+    ):
         category.METHOD
+
 
 def test_collection_exists():
     collection = AppStoreCollections()
-    assert collection.NEW_IOS == 'newapplications'
+    assert collection.NEW_IOS == "newapplications"
+
 
 def test_collection_does_not_exist():
     collection = AppStoreCollections()
-    with pytest.raises(AttributeError, match="'AppStoreCollections' object has no attribute 'NOTHING'"):
+    with pytest.raises(
+        AttributeError, match="'AppStoreCollections' object has no attribute 'NOTHING'"
+    ):
         collection.NOTHING
+
 
 def test_app_utils():
     utils = AppStoreUtils()
